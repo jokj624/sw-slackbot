3bd11530e3438a12677f61641b897c47cac22e87
jokj624 <jokj624@gmail.com>
2021-04-20 18:22:54 +0900

    feat : 크롤링 기능 및 메시지 전송 추가

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,33 +1,44 @@
 import { WebClient } from '@slack/web-api';
 import Router from 'koa-router';
 import CONFIG from '../../config/bot.json';
+import { crawling  } from '../crawl/crawling';
 
-const token = CONFIG.BOT_USER_OAUTH_ACCESS_TOKEN;
+const token : string = CONFIG.BOT_USER_OAUTH_ACCESS_TOKEN;
+const verification : string = CONFIG.VERIFICATION_TOKEN;
 
 const api = new Router();
 const web = new WebClient(token);
 
-api.post('/slack/events', (ctx) => {
-    const body = ctx.request.body;
-    const event = body.event;
-
-    if(body.type === 'event_callback'){
-        //메세지 이벤트
-        if(event.text === '가져와'){
-            console.log(`수신 channel:${event.channel}, user: ${event.user}`);
-            web.chat.postMessage({
-                channel: event.channel,
-                text: '가져올게요'
-            }).then(result => {
-                console.log('메세지 발신 : ' + result.ts);
-            });
-            ctx.status = 200;
-        }
-    } else if(body.type === 'url_verification'){
+api.post('/slack/events', async(ctx : { body: any , status: number, request: any}) => {
+    const body : any = ctx.request.body;
+    if(body.type === 'url_verification'){
         //url 검증
-        console.log('url verification');
         ctx.body = body.challenge;
-    } else {
+        ctx.status = 200;
+    } 
+});
+api.post('/slack/events/command', async (ctx : { body: any , status: number, request: any}) => {
+    const body : any = ctx.request.body;
+    if (body.token === verification) {
+        const textResult = await crawling();
+        let textBlock: any[] = [{type:"section", text:{type:"mrkdwn", text: "소프트웨어학부 취업 공지사항 입니다."}}];
+        textResult.map((item: any) => {
+            const obj : any = {
+                type: "section",
+                text: {
+                    type:"mrkdwn",
+                    text: "*"+ item.title + "*\n" + item.urlInfo
+                }
+            };
+            const divider : any = { type: "divider" };
+            textBlock.push(obj);
+            textBlock.push(divider);
+        });
+        web.chat.postMessage({
+            channel: body.channel_id,
+            blocks: textBlock
+        });
+        ctx.body = body.text;
         ctx.status = 200;
     }
 });
---
+++ src/crawl/crawling.ts
@@ -0,0 +1,16 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+export const crawling = async () => {
+    let textList: any = []; 
+    const html = await axios.get("https://software.sookmyung.ac.kr/software/5911/subview.do");
+    const $ = cheerio.load(html.data);
+    const $bodyList = $("li.thumbLi").children("a.artclLinkView");
+    $bodyList.each((i : any, elem: any) => {
+        textList[i] = {
+            title: $(elem).find('div.artclTitle strong').text(),
+            urlInfo: "https://software.sookmyung.ac.kr"+ $(elem).attr('href')
+        };
+    }); 
+    return textList;
+}
